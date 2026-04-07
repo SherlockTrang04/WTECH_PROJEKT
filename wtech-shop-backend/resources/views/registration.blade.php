5ab790ea60e527ede041add51ae4e2ac7befbad7
SherlockTrang04 <hihowhe.di2000@gmail.com>
2026-04-07 14:45:21 +0200

    small changes

--- wtech-shop-backend/resources/views/registration.blade.php
+++ wtech-shop-backend/resources/views/registration.blade.php
@@ -25,7 +25,7 @@
             <label for="email">Email:</label>
             <input type="email" id="email" name="email" required><br><br>
 
-            <label for="password">Heslo:</label>
+            <label for="password">Heslo (minimalne 8 znakov):</label>
             <input type="password" id="password" name="password" required><br><br>
 
             <button type="submit">Registrovať sa</button>
