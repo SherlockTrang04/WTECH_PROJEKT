1cf1f3f0a71380eeb7781e6b9b4de96e724a1356
Mykola Malesh <mukolamalesh@gmail.com>
2026-04-09 23:08:15 +0200

    add products table

---
+++ wtech-shop-backend/database/migrations/2026_04_09_201918_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->string('name');
+            $table->text('description');
+            $table->tsvector('search_vector');
+            $table->smallInteger('stars')->default(0)->nullable();
+            $table->string('brand');
+            $table->string('color');
+            $table->unsignedInteger('stock');
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
