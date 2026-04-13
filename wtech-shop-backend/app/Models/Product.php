6be0ba5c2f0c1670d821bd16789a6499f5d3d68e
Mykola Malesh <mukolamalesh@gmail.com>
2026-04-14 00:07:10 +0200

    add models

--- wtech-shop-backend/app/Models/Product.php
+++ wtech-shop-backend/app/Models/Product.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'category_id', 'name', 'description', 'stars',
+      'brand', 'color', 'stock', 'is_active', 'price'
+    ];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function images() {
+        return $this->hasMany(ProductImage::class);
+    }
 }
